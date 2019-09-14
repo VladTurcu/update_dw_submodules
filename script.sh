b388480ee6d1dbf08728673432d75d0cc812bf92
Vlad Turcu <turcuvlad87@gmail.com>
2019-09-14 21:54:50 +0100

    your message

---
+++ script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+RED=`echo "\e[31m"`
+GREEN='\e[0;32m'
+NC='echo \033[0m'
+
+function update_submodule () {
+    cd $1
+    # if [[ "$CURRENT_BRANCH" -ne "develop" ]]; then
+    #       echo "Fatal: Current branch is not master"
+    #       return;      # I need to abort here!
+    # fi
+    echo -e "$GREEN Running yarn in folder $NC $1"
+    cd ..
+}
+
+for d in *; do
+    if [[ -d $d ]]; 
+    then
+        update_submodule $d
+    fi
+done
+
+git init
+git add .
+git commit -m "your message"
+git branch develop
+git checkout develop
